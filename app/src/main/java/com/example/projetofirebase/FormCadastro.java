dd4344d71709a54c1dd1963a92a0496b289bd353
KaioFerreira1 <kaiof2902@gmail.com>
2023-04-06 17:29:52 -0300

    upload project

---
+++ app/src/main/java/com/example/projetofirebase/FormCadastro.java
@@ -0,0 +1,15 @@
+package com.example.projetofirebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FormCadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_cadastro);
+        getSupportActionBar().hide();
+    }
+}
